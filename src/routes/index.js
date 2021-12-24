34364a3a922574170c50c195e2aa566bbcbf21e7
Emmaggok <emmanuelblackgg@gmail.com>
2021-12-23 22:00:50 -0300

    minor change

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const {dnaMutation} = require('../controllers/dnaController');
 const router = Router();
 
 router.post('/mutation', dnaMutation)
+router.get('/', (req, res) => res.send('Welcome to the DNA´S verification API'))
 
 module.exports = router;
